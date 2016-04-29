6f97c12a58bc18cc717c24663eccc6a211e8a1e9
Sergey Danilov <sergeydanilov@gmail.com>
2016-04-29 13:30:00 +0800

    added student parsable object

---
+++ HelloWorldApp/app/src/main/java/hello/sample/mobile/bpal/ru/helloworldapp/data/Student.java
@@ -0,0 +1,52 @@
+package hello.sample.mobile.bpal.ru.helloworldapp.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ *
+ */
+public class Student implements Parcelable {
+    private String id;
+    private String name;
+    private String grade;
+
+    // Constructor
+    public Student(String id, String name, String grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+    // Parcelling part
+    public Student(Parcel in){
+        String[] data = new String[3];
+
+        in.readStringArray(data);
+        this.id = data[0];
+        this.name = data[1];
+        this.grade = data[2];
+    }
+
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {this.id,
+                this.name,
+                this.grade});
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Student createFromParcel(Parcel in) {
+            return new Student(in);
+        }
+
+        public Student[] newArray(int size) {
+            return new Student[size];
+        }
+    };
+}
