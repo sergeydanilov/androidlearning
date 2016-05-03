97b9e687184ae7ec8118c5aaa60c6f46331ad5b1
Sergey Danilov <sergeydanilov@gmail.com>
2016-05-03 14:35:24 +0800

    added content provider example sceleton

---
+++ TPProviderDemo/app/src/main/java/cities/sample/mobile/bpal/ru/tpproviderdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package cities.sample.mobile.bpal.ru.tpproviderdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
