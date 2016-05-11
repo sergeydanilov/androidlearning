c00418d60db9d0b60b9dee130ffe3c55bd47b1dd
Sergey Danilov <sergeydanilov@gmail.com>
2016-05-11 15:15:56 +0800

    added web activity

---
+++ HelloWorldApp/app/src/main/java/hello/sample/mobile/bpal/ru/helloworldapp/activity/WebActivity.java
@@ -0,0 +1,15 @@
+package hello.sample.mobile.bpal.ru.helloworldapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hello.sample.mobile.bpal.ru.helloworldapp.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
