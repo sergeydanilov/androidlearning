b32f33e56bbf703d2cd3e66707c5998a010cf0f9
Sergey Danilov <sergeydanilov@gmail.com>
2016-05-11 15:30:56 +0800

    fixed web view 2

--- HelloWorldApp/app/src/main/java/hello/sample/mobile/bpal/ru/helloworldapp/activity/WebActivity.java
+++ HelloWorldApp/app/src/main/java/hello/sample/mobile/bpal/ru/helloworldapp/activity/WebActivity.java
@@ -2,6 +2,7 @@ package hello.sample.mobile.bpal.ru.helloworldapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import hello.sample.mobile.bpal.ru.helloworldapp.R;
@@ -14,6 +15,10 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
         myWebView.loadUrl("http://www.google.com");
     }
 }
